13f7e790dbe7026d4a84d0cf497c0954037c4e6e
Emelie Sidesiö <esidesio@gmail.com>
2017-12-04 14:01:28 +0100

    pause and start button

--- code/src/index.js
+++ code/src/index.js
@@ -25,11 +25,26 @@ class Clock extends React.Component {
     })
   }
 
+  pauseClick = () => { clearInterval(this.timerID) }
+
+  startClick = () => {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    )
+  }
+
   render() {
     return (
       <div>
         <h1>Hello, world!</h1>
         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <button onClick={this.pauseClick}>
+        Pause
+        </button>
+        <button onClick={this.startClick}>
+        Start
+        </button>
       </div>
     )
   }
